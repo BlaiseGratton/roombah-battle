62d7e7ee863a4d6d27ac40c692f31b0bf1fcdeab
BlaiseGratton <blaisegratton@gmail.com>
2015-08-16 12:19:27 -0500

    added gulp for managing front end builds

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+     del = require('del');
+
+gulp.task("concatScripts", function() {
+    return gulp.src([
+        'src/js/app.js',
+        'src/js/*.js',
+        'src/js/**/*.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('minifyScripts', ['concatScripts'], function() {
+  return gulp.src("public/js/app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('compileSass', function() {
+  return gulp.src("src/styles/main.scss")
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('public/styles'));
+});
+
+gulp.task('transferViews', function() {
+  return gulp.src("src/views/*.html")
+      .pipe(gulp.dest('public/views'));
+});
+
+gulp.task('copyIndex', function() {
+  return gulp.src("src/index.html")
+      .pipe(gulp.dest('public'));
+});
+
+gulp.task('watchFiles', function() {
+  gulp.watch(['src/styles/**/*.scss', 'src/styles/*.scss'], ['compileSass']);
+  gulp.watch(['src/js/*.js', 'src/js/**/*.js'], ['concatScripts']);
+  gulp.watch(['src/*.html'], ['copyIndex']);
+  gulp.watch(['src/views/**/*.html'], ['transferViews']);
+});
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task('clean', function() {
+  del(['public/js', 'public/styles']);
+});
+
+gulp.task("build", [
+    'clean', 
+    'minifyScripts', 
+    'compileSass'
+    ], function() {
+      return gulp.src(["styles/main.css", "js/app.min.js"], { base: './'})
+            .pipe(gulp.dest('public'));
+});
+
+gulp.task("default", ["serve"]);
---
+++ public/js/app.js
@@ -0,0 +1,154 @@
+'use stict';
+
+var app = angular.module('roombah-battle', []);
+
+
+app.controller('mainController', function(socket, $scope, positionService, $interval){
+
+  var vm = this;
+
+  socket.on('top', function(y) {
+    vm.top = y;
+  });
+
+  socket.on('right', function(x) {
+    vm.right = x;
+  });
+
+  $scope.roombah = {};
+  $scope.roombah.top = 0;
+  $scope.roombah.right = 0;
+  $scope.roombah.goingDown = true;
+  $scope.roombah.goingRight = false;
+  $scope.roombah.color = 'violet';
+
+  $interval(function() {
+    $scope.roombah = positionService.calculateMovement($scope.roombah);
+  }, 20);
+
+  socket.on('otherRoombahs', function(roombah) {
+    roombahIndex = vm.roombahNames.indexOf(roombah.name);
+    if(roombahIndex > -1) {
+      vm.otherRoombahs[roombahIndex] = roombah;
+    } else {
+      vm.roombahNames.unshift(roombah.name);
+      vm.otherRoombahs.unshift(roombah);
+    }
+  });
+
+  $scope.$watch('roombah.top', function(y) {
+    socket.emit('otherRoombahs', $scope.roombah);
+  });
+
+  $scope.$watch('roombah.right', function(x) {
+    socket.emit('otherRoombahs', $scope.roombah);
+  });
+
+  vm.roombahNames = [];
+
+  vm.otherRoombahs = [];
+});
+
+app.factory('positionService', function() {
+
+  var service = {};
+
+  var changeYDirection = function(roombah) {
+    roombah.goingDown = !roombah.goingDown;
+  }
+
+  var changeXDirection = function(roombah) {
+    roombah.goingRight = !roombah.goingRight;
+  }
+
+  var checkArenaBounds = function(roombah) {
+    if (roombah.top === 0 || roombah.top === 270) {
+      changeYDirection(roombah);
+    }
+    
+    if (roombah.right === 0 || roombah.right === 290) {
+      changeXDirection(roombah);
+    }
+  };
+
+  service.calculateMovement = function(roombah) {
+    if (roombah.goingDown) {
+      roombah.top++;
+    } else {
+      roombah.top--;
+    }
+    if (roombah.goingRight) {
+      roombah.right--;
+    } else {
+      roombah.right++;
+    }
+    checkArenaBounds(roombah);
+    return roombah;
+  };
+
+  return service;
+});
+
+app.controller('roombahController', function(socket) {
+  var vm = this;
+});
+
+app.directive('roombah', function(socket) {
+  return {
+    restrict: 'E',
+    template: '<div class="roombah">{{ roombah.name }}</div>', 
+    replace: true,
+    bindToController: {
+      'top': '=',
+      'right': '=',
+      'color': '=',
+      'name': '='
+    },
+    controller: 'roombahController',
+    controllerAs: 'rmbCtrl',
+    link:function (scope, element, attrs) {
+      scope.$watch(attrs.top, function (y) {
+        element.css('top', y + 'px');
+        socket.emit('top', y);
+      });
+      scope.$watch(attrs.right, function (x) {
+        element.css('right', x + 'px');
+        socket.emit('right', x);
+      });
+      scope.$watch(attrs.color, function (color) {
+        element.css('backgroundColor', color);
+      });
+    }
+  };
+});
+
+app.factory('socket', function($rootScope) {
+
+  var socket = io.connect();
+
+  service = {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+
+  return service;
+});
+
+
+//# sourceMappingURL=app.js.map
