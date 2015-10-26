c719b2d4874f3ed309cf82f0f53ef30cadf9e66b
BlaiseGratton <blaisegratton@gmail.com>
2015-10-26 14:52:32 -0500

    better test beforeEach() setup, passing tests for wall bouncing

--- test/collisionTests.js
+++ test/collisionTests.js
@@ -4,7 +4,7 @@ var collisions = require('../app/collisions.js');
 
 var roomba, roomba1, roomba2, roomba3;
 
-describe('Simple Tests:', function() {
+describe('Collision Tests:', function() {
 beforeEach(function() {
   roomba = {
     y: 100,
@@ -51,71 +51,68 @@ beforeEach(function() {
     roomba3 = collisions.calculateDirectionAndSpeedFromXYVectors(roomba3);
   });
 
-
-  describe('Simple test', function() {
-    it('should run a simple test', function() {
-      assert.equal(false, false);
-    });
-
-    it('should fail here', function() {
-      assert.equal(false, false);
-    });
-  });
-});
-
-describe('Setting Direction and Speed from Vectors of Roomba:', function() {
-  describe('Setting vectors on', function() {
-    it('roomba', function() {
-      assert.equal(0.7853981633974483, roomba.direction);
-      assert.equal(1.4142135623730951, roomba.speed);
-    });
-    it('roomba1', function() {
-      assert.equal(3.9269908169872414, roomba1.direction);
-      assert.equal(1.4142135623730951, roomba1.speed);
-    });
-    it('roomba2', function() {
-      assert.equal(1.5208379310729538, roomba2.direction);
-      assert.equal(2.0024984394500787, roomba2.speed);
-    });
-    it('roomba3', function() {
-      assert.equal(3.338988213439674, roomba3.direction);
-      assert.equal(0.5099019513592785, roomba3.speed);
+  describe('Setting Direction and Speed from Vectors of Roomba:', function() {
+    describe('Setting vectors on', function() {
+      it('roomba', function() {
+        assert.equal(0.7853981633974483, roomba.direction);
+        assert.equal(1.4142135623730951, roomba.speed);
+      });
+      it('roomba1', function() {
+        assert.equal(3.9269908169872414, roomba1.direction);
+        assert.equal(1.4142135623730951, roomba1.speed);
+      });
+      it('roomba2', function() {
+        assert.equal(1.5208379310729538, roomba2.direction);
+        assert.equal(2.0024984394500787, roomba2.speed);
+      });
+      it('roomba3', function() {
+        assert.equal(3.338988213439674, roomba3.direction);
+        assert.equal(0.5099019513592785, roomba3.speed);
+      });
     });
   });
-});
 
-describe('Bouncing Roombas off of Walls:', function() {
-  describe('On the side wall', function() {
-    it('should correctly change the direction', function() {
-      roomba = collisions.bounceOffSides(roomba);
-      assert.equal(5.497787143782138, roomba.direction);
+  describe('Bouncing Roombas off of Side Walls:', function() {
+    describe('On the side wall', function() {
+      it('should correctly change the direction', function() {
+        roomba = collisions.bounceOffSides(roomba);
+        assert.equal(5.497787143782138, roomba.direction);
 
-      roomba1 = collisions.bounceOffSides(roomba1);
-      assert.equal(2.356194490192345, roomba1.direction);
+        roomba1 = collisions.bounceOffSides(roomba1);
+        assert.equal(2.356194490192345, roomba1.direction);
 
-      roomba2 = collisions.bounceOffSides(roomba2);
-      assert.equal(4.762347376106632, roomba2.direction);
+        roomba2 = collisions.bounceOffSides(roomba2);
+        assert.equal(4.762347376106632, roomba2.direction);
 
-      roomba3 = collisions.bounceOffSides(roomba3);
-      console.log(roomba3.xVelocity);
-      assert.equal(2.9441970937399122, roomba3.direction);
+        roomba3 = collisions.bounceOffSides(roomba3);
+        assert.equal(2.9441970937399122, roomba3.direction);
+      });
     });
   });
-  describe('On the top or bottom wall', function() {
-    it('should correctly change the direction', function() {
 
+  describe('Bouncing Roombas off of Top or Bottom Walls:', function() {
+    describe('On the top or bottom wall', function() {
+      it('should correctly change the direction', function() {
+        roomba = collisions.bounceOffTopOrBottom(roomba);
+        assert.equal(2.356194490192345, roomba.direction);
+
+        roomba1 = collisions.bounceOffTopOrBottom(roomba1);
+        assert.equal(5.497787143782138, roomba1.direction);
+
+        roomba2 = collisions.bounceOffTopOrBottom(roomba2);
+        assert.equal(1.6207547225168393, roomba2.direction);
+
+        roomba3 = collisions.bounceOffTopOrBottom(roomba3);
+        assert.equal(6.085789747329706, roomba3.direction);
+      });
     });
   });
-});
 
-
-
-
-
-describe('convertVectorsToSpeed:', function() {
-  describe('it', function() {
-    it('should return the calculated speed', function() {
-      assert.equal(20, 20);
+  describe('convertVectorsToSpeed:', function() {
+    describe('it', function() {
+      it('should return the calculated speed', function() {
+        assert.equal(20, 20);
+      });
     });
   });
 });
